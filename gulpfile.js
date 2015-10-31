716038f17172ba2050360323ae6abdbc3dd91e74
Farzad Seid Arabi <farzad.seid.arabi@gmail.com>
2015-10-31 21:58:15 +0330

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', ['build']);
-gulp.task('build', ['sass']);
 gulp.task('sass', function(done) {
     gulp.src('scss/base.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./release/css/'));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./scss/**/*.scss', ['sass']);
-});
+// Watch for changes in files
+gulp.task('watch', function() {
+   // Watch .scss files
+  gulp.watch('./scss/**/*.scss', ['sass']);   
+ });
+
+gulp.task('default', ['sass', 'watch']);
