ecd43a6cb5dbf0d9e1657e40f144d0fd28d85010
Farzad Seid Arabi <farzad.seid.arabi@gmail.com>
2015-11-05 17:16:28 +0330

    update package.json, gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require("gulp-concat");
 
 gulp.task('sass', function(done) {
     gulp.src('scss/base.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./release/css/'));
+
+    gulp.src('scripts/*.js') // path to your files		
+		.pipe(concat('fast.js'))
+		.pipe(gulp.dest('./release/js/'));
 });
+
 
 // Watch for changes in files
 gulp.task('watch', function() {
